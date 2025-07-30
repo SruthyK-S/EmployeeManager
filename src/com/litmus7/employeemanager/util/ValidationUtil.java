a407abb3c99edecb46fd2b11371506362c95b2c4
Sruthy K S <141996444+SruthyK-S@users.noreply.github.com>
2025-07-30 09:29:24 +0530

    Update ValidationUtil.java
    Added duplicate checking for ID, Mobile Number and email
    Added before limit for Joining date

--- src/com/litmus7/employeemanager/util/ValidationUtil.java
+++ src/com/litmus7/employeemanager/util/ValidationUtil.java
@@ -1,21 +1,107 @@
 package com.litmus7.employeemanager.util;
 import java.time.LocalDate;
 import java.util.HashSet;
+import java.io.*;
 
 import com.litmus7.employeemanager.dto.Employee;
 
 public class ValidationUtil 
 {
-	HashSet<Integer> idSet = new HashSet<>();
+
+	String outputFilePath;
+	
+	public ValidationUtil(String outputFilePath)
+	{
+		this.outputFilePath = outputFilePath;
+		
+	}
+	
+	
+	public boolean isDuplicateID(int newID, String outputFilePath) 
+	{
+		
+	    try (BufferedReader reader = new BufferedReader(new FileReader(outputFilePath))) 
+	    {
+	        String line;
+	        reader.readLine();
+
+	        while ((line = reader.readLine()) != null) {
+	            String[] parts = line.split(",");
+	            if (parts.length > 0) {
+	                int existingID = Integer.parseInt(parts[0].trim());
+	                if (existingID == newID) 
+	                {
+	                    return true; 
+	                }
+	            }
+	        }
+
+	    } catch (IOException | NumberFormatException e) {
+	        e.printStackTrace();
+	    }
+
+	    return false; 
+	}
+	
+	public boolean isDuplicateMobile(String mobile) 
+	{
+	    try (BufferedReader reader = new BufferedReader(new FileReader(this.outputFilePath))) 
+	    {
+	        String line;
+	        reader.readLine();
+
+	        while ((line = reader.readLine()) != null) {
+	            
+	            String[] parts = line.split(",");
+	            if (parts.length > 0) {
+	                String existingNumber = parts[3];
+	                if (existingNumber.equals(mobile)) {
+	                    return true; 
+	                }
+	            }
+	        }
+
+	    } catch (IOException | NumberFormatException e) {
+	        e.printStackTrace();
+	    }
+
+	    return false; 
+	}
+	
+	public boolean isDuplicateEmail(String email) 
+	{
+	    try (BufferedReader reader = new BufferedReader(new FileReader(this.outputFilePath))) 
+	    {
+	        String line;
+	        reader.readLine();
+
+	        while ((line = reader.readLine()) != null) {
+
+	            String[] parts = line.split(",");
+	            if (parts.length > 0) {
+	                String existingEmail = parts[4];
+	                if (existingEmail.equals(email)) {
+	                    return true; 
+	                }
+	            }
+	        }
+
+	    } catch (IOException | NumberFormatException e) {
+	        e.printStackTrace();
+	    }
+
+	    return false; 
+	}
+	
 	
 	public String Validate(Employee emp)
 	{
 		String message = "valid";
 		
 		int ID = emp.getID();
-		if (idSet.contains(ID)) 
+		if (isDuplicateID(ID, outputFilePath)) 
 		{
-		    message = "Duplicate ID! This ID already exists.";
+		    message = "Duplicate ID! This ID already exists";
 		    return message;
 		    
 		} 
@@ -23,7 +109,7 @@ public class ValidationUtil
 		String firstName = emp.getFirstName();
 		if(firstName.isEmpty())
 		{
-			message = "First Name cannot be empty.";
+			message = "First Name cannot be empty";
 			return message;
 		}
 		
@@ -31,7 +117,7 @@ public class ValidationUtil
 		String lastName = emp.getLastName();
 		if(lastName.isEmpty())
 		{
-			message = "Last Name cannot be empty.";
+			message = "Last Name cannot be empty";
 			return message;
 		}
 		
@@ -41,6 +127,10 @@ public class ValidationUtil
 		    message = "Invalid mobile number. Must be 10 digits.";
 		    return message;
 		}
+		else if(isDuplicateMobile(mobile))
+		{
+			message = "Duplicate mobile number.";
+		}
 		
 		String email = emp.getEmail();
 		if(!email.matches("^[\\w.-]+@[\\w.-]+\\.[a-zA-Z]{2,}"))
@@ -48,17 +138,22 @@ public class ValidationUtil
 			message = "Invalid email address.";
 			return message;
 		}
+		else if(isDuplicateEmail(email))
+		{
+			message = "Duplicate email address";
+		}
 		
 		boolean active = emp.isActive();
 		
 		LocalDate date = emp.getJoiningDate();
 		LocalDate today = LocalDate.now();
-		if(date.isAfter(today))
+		LocalDate establishedDate = LocalDate.of(2009, 6, 30);
+		if(date.isAfter(today) || date.isBefore(establishedDate))
 		{
 			message = "Invalid joining date.";
 			return message;
 		}
-		idSet.add(ID);
+		
 		return message;
 	}
 }
