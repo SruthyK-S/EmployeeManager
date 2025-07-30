8403338e1c9057a673f50b9b02f2238f0276ab1a
Sruthy K S <141996444+SruthyK-S@users.noreply.github.com>
2025-07-30 09:25:23 +0530

    Update EmployeeManagerApp.java

--- src/com/litmus7/employeemanager/app/EmployeeManagerApp.java
+++ src/com/litmus7/employeemanager/app/EmployeeManagerApp.java
@@ -20,67 +20,80 @@ public class EmployeeManagerApp {
 		
 		String option = "";
 		String result = "";
+		System.out.println("Employee Management App");
 		
 		while(!option.equals("q") && !option.equals("Q"))
 		{
-			System.out.println("Choose one option...");
+			System.out.println("Choose one service");
 			System.out.println("1 - Read data from text file and add it to database"
-					+ "\n2 - Enter data from console \n'q' - quit");
+					+ "\n2 - Enter data from console"
+					+ "\n3 - Load data from output File"
+					+ "\n'q' - quit");
 			
 		    option = scn.next();  
+		    switch(option) {
 		    
-		    if(option.equals("1"))
-		    {
-//		    	System.out.println("");
-		    	result = empCon.dataIngestion();
-		    	System.out.println(result);
-		    	
-		    }
-		    else if(option.equals("2"))
-	    	{
-		    	System.out.println("Enter the employee details seperated by comma");
-		    	System.out.println("ID: ");
-		    	int ID = scn.nextInt();
-		    	
-		    	System.out.println("First Name: ");
-		    	String firstName = scn.next();
 		    
-		    	
-		    	System.out.println("Last Name: ");
-		    	String lastName = scn.next();
-		    	
-		    	System.out.println("Mobile Number: ");
-		    	String mobile = scn.next();
-		    	
-		    	System.out.println("Email address: ");
-		    	String email = scn.next();
-		    	
-		    	System.out.println("Joining date(yyyy-MM-dd): ");
-		    	String date = scn.next();
-		    	DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-		    	LocalDate joiningDate = LocalDate.parse(date, formatter);
-		        
-		    	
-		    	System.out.println("Active Status(T/F): ");
-		    	String activeStatus = scn.next();
-		    	boolean active = activeStatus.equals("T")? true : false;
-		    	
-		    	
-		    	Employee emp = new Employee(ID, firstName, lastName, mobile, email, joiningDate, active);
-	    		result = empCon.dataEntry(emp, outputFilePath);
-	    		System.out.println("data entry" + result);
-	    	}
-		    else
-		    {
-		    	System.out.println("Exited the application");
+			    case "1":
+			    {
+	//		    	System.out.println("");
+			    	result = empCon.dataIngestion();
+			    	System.out.println(result);
+			    	break;
+			    	
+			    }
+			    case "2":
+		    	{
+			    	System.out.println("Enter the employee details seperated by comma");
+			    	System.out.println("ID : ");
+			    	int ID = scn.nextInt();
+			    	
+			    	System.out.println("First Name : ");
+			    	String firstName = scn.next();
+			    
+			    	
+			    	System.out.println("Last Name : ");
+			    	String lastName = scn.next();
+			    	
+			    	System.out.println("Mobile Number : ");
+			    	String mobile = scn.next();
+			    	
+			    	System.out.println("Email address : ");
+			    	String email = scn.next();
+			    	
+			    	System.out.println("Joining date(yyyy-MM-dd) : ");
+			    	String date = scn.next();
+			    	DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+			    	LocalDate joiningDate = LocalDate.parse(date, formatter);
+			        
+			    	
+			    	System.out.println("Active Status(T/F) : ");
+			    	String activeStatus = scn.next();
+			    	boolean active = activeStatus.equals("T")? true : false;
+			    	
+			    	
+			    	Employee emp = new Employee(ID, firstName, lastName, mobile, email, joiningDate, active);
+		    		result = empCon.dataEntry(emp, outputFilePath);
+		    		System.out.println(result);
+		    		break;
+		    	}
+			    case "3": 
+			    {
+			    	result = empCon.loadData();
+			    	System.out.println(result);
+			    	break;
+			    }
+			    default: 
+			    {
+			    	System.out.println("Exited the application");
+			    }
 		    }
 		    
 		}
 		
-			    
+		
 		scn.close();	    
 		    
 		}
 	    
 }
-
