6bf7ab804d2a373c3c0225a9b03ac13f2bc8c1f1
Sruthy K S <141996444+SruthyK-S@users.noreply.github.com>
2025-08-07 12:06:30 +0530

    Update DBConnectionUtil.java

--- src/com/litmus7/employeemanager/util/DBConnectionUtil.java
+++ src/com/litmus7/employeemanager/util/DBConnectionUtil.java
@@ -7,10 +7,11 @@ import java.sql.SQLException;
 public class DBConnectionUtil {
 
     private static final String URL = "jdbc:mysql://localhost:3306/employeedatabase";
-    private static final String USER = "student";
-    private static final String PASSWORD = "student";
+    private static final String USER = "";
+    private static final String PASSWORD = "";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
     }
 }
+
