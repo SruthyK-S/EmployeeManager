a7be31fe79b7c7b9e2b270b547b1a1d5c6499a33
Sruthy K S <141996444+SruthyK-S@users.noreply.github.com>
2025-08-07 11:58:37 +0530

    Update EmployeeService.java

--- src/com/litmus7/employeemanager/service/EmployeeService.java
+++ src/com/litmus7/employeemanager/service/EmployeeService.java
@@ -1,14 +1,16 @@
 package com.litmus7.employeemanager.service;
-import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
 import com.litmus7.employeemanager.dto.Employee;
 import com.litmus7.employeemanager.dto.Response;
+import com.litmus7.employeemanager.util.ValidationUtil;
 import com.litmus7.employeemanager.dao.EmployeeDAO;
 
 
 public class EmployeeService {
 	EmployeeDAO dao = new EmployeeDAO();
+	ValidationUtil val = new ValidationUtil();
 	
 	public int createEmployee(Employee  emp)
 	{
@@ -42,6 +44,50 @@ public class EmployeeService {
 	{
 
 		return dao.updateEmployee(emp);
+	}
+	
+	
+	public Response<Integer> checkIfDataValid(String ID, String firstName, String lastName, 
+			String mobile, String email, String dateStr, String active, HashSet<String> idSet)
+	{
+		String message;
+		if( idSet.contains(ID) )
+			return new Response<>(false,  "Duplicate ID!", 0);
+		message = val.isValidID(ID);
+		if(!message.equals("valid"))
+			return new Response<>(false, message, 0);
+		
+		
+		message = val.isValidName(firstName);
+		if(!message.equals("valid"))
+		return new Response<>(false, message, 0);
+		
+		
+		message = val.isValidName(lastName);
+		if(!message.equals("valid"))
+		return new Response<>(false, message, 0);
+		
+		
+		message = val.isValidNumber(mobile);
+		if(!message.equals("valid"))
+		return new Response<>(false, message, 0);
+		
+		
+		message = val.isValidEmail(email);
+		if(!message.equals("valid"))
+		return new Response<>(false, message, 0);
+		
+		
+		message = val.isValidJoiningDate(dateStr);
+		if(!message.equals("valid"))
+		return new Response<>(false, message, 0);
+		
+		message = val.isValidActiveStatus(active);
+		if(!message.equals("valid"))
+		return new Response<>(false, message, 0);
+		
+		return new Response<>(true, "valid" , 1);
+		
 	}
 	
 }
