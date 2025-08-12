eb1d2c9f8631842976f61c19360c6a35eb3da0e2
Sruthy K S <141996444+SruthyK-S@users.noreply.github.com>
2025-08-12 12:09:40 +0530

    Create EmployeeDaoException.java

---
+++ src/com/litmus7/employeemanager/exceptions/EmployeeDaoException.java
@@ -0,0 +1,11 @@
+package com.litmus7.employeemanager.exceptions;
+
+public class EmployeeDaoException extends Exception {
+
+	
+	private static final long serialVersionUID = 1L;
+
+	public EmployeeDaoException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
