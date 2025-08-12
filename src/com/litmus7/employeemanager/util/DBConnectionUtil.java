89a79e4afb3f215665f034874871ce59335c7fe3
Sruthy K S <141996444+SruthyK-S@users.noreply.github.com>
2025-08-12 12:06:18 +0530

    Update DBConnectionUtil.java

--- src/com/litmus7/employeemanager/util/DBConnectionUtil.java
+++ src/com/litmus7/employeemanager/util/DBConnectionUtil.java
@@ -3,15 +3,35 @@ package com.litmus7.employeemanager.util;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
+import java.io.InputStream;
+import java.io.IOException;
 
 public class DBConnectionUtil {
 
-    private static final String URL = "jdbc:mysql://localhost:3306/employeedatabase";
-    private static final String USER = "";
-    private static final String PASSWORD = "";
+    private static String URL;
+    private static String USER;
+    private static String PASSWORD;
+
+    // Static block loads properties when the class is first loaded
+    static {
+        try (InputStream input = DBConnectionUtil.class.getClassLoader().getResourceAsStream("dbconfig.properties")) {
+            Properties prop = new Properties();
+            if (input == null) {
+                throw new RuntimeException("Unable to find dbconfig.properties");
+            }
+            prop.load(input);
+
+            URL = prop.getProperty("db.url");
+            USER = prop.getProperty("db.user");
+            PASSWORD = prop.getProperty("db.password");
+
+        } catch (IOException e) {
+            throw new RuntimeException("Error reading database configuration", e);
+        }
+    }
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
     }
 }
-
