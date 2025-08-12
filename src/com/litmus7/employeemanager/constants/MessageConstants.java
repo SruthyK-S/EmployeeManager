ca02c0a17e3d54e3716049766b1632de7c00db6e
Sruthy K S <141996444+SruthyK-S@users.noreply.github.com>
2025-08-12 12:11:22 +0530

    Create MessageConstants.java

---
+++ src/com/litmus7/employeemanager/constants/MessageConstants.java
@@ -0,0 +1,17 @@
+package com.litmus7.employeemanager.constants;
+
+public class MessageConstants {
+
+    public static final String INVALID_FILE_PATH = "Invalid input file path!";
+
+    public static final String ID_NOT_FOUND = "ID doesn't exist.";
+
+    public static final String DATA_RETRIEVED = "Data retrieved successfully.";
+    
+    public static final String FAILED_TO_PROCESS = "Failed to process the request.";
+
+    public static final String EMPTY_ID = "Employee ID cannot be empty.";
+    
+    public static final String INPUT_FILE_PATH_EMPTY = "Input file path cannot be empty!";
+
+}
