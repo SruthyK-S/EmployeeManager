91b97210cd51b08fd66c04f362af8fdf802810e4
Sruthy K S <141996444+SruthyK-S@users.noreply.github.com>
2025-08-13 11:17:57 +0530

    Add files via upload

---
+++ src/com/litmus7/employeemanager/exceptions/ValidationException.java
@@ -0,0 +1,10 @@
+package com.litmus7.employeemanager.exceptions;
+
+public class ValidationException extends Exception{
+
+	private static final long serialVersionUID = 1L;
+
+	public ValidationException(String message) {
+        super(message);
+    }
+}
