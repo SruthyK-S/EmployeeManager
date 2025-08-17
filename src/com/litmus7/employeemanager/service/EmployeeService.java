53e0d361b59b3a135a395d667f742c040da75ac6
Sruthy K S <141996444+SruthyK-S@users.noreply.github.com>
2025-08-17 20:27:05 +0530

    Update EmployeeService.java

--- src/com/litmus7/employeemanager/service/EmployeeService.java
+++ src/com/litmus7/employeemanager/service/EmployeeService.java
@@ -1,27 +1,30 @@
 package com.litmus7.employeemanager.service;
-import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
 import com.litmus7.employeemanager.dto.Employee;
 import com.litmus7.employeemanager.exceptions.EmployeeDaoException;
 import com.litmus7.employeemanager.exceptions.EmployeeNotFoundException;
+//import static com.litmus7.employeemanager.constants.ErrorCodeConstants.ERROR_CODES;
 import com.litmus7.employeemanager.exceptions.EmployeeServiceException;
 import com.litmus7.employeemanager.exceptions.ValidationException;
-import com.litmus7.employeemanager.util.TextFileUtil;
 import com.litmus7.employeemanager.util.ValidationUtil;
 import com.litmus7.employeemanager.dao.EmployeeDAO;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 
 public class EmployeeService {
-	private EmployeeDAO dao = new EmployeeDAO();
+	private static final Logger logger = LogManager.getLogger(EmployeeService.class);
+	
+	private EmployeeDAO employeeDao = new EmployeeDAO();
 	
 	public int createEmployee(Employee  emp) throws EmployeeServiceException
 	{
+		logger.trace("Entering service layer createEmployee() with ID: {}", emp.getID());
 		try {
-			return dao.saveEmployee(emp);
+			return employeeDao.saveEmployee(emp);
         } catch (EmployeeDaoException e) {
-            throw new EmployeeServiceException("Service layer failed to save employee details.", e);
+            throw new EmployeeServiceException(e.getMessage(), e.getErrorCode(), e);
         }
 		
 			
@@ -30,17 +33,18 @@ public class EmployeeService {
 	public List<Employee> getAllEmployees() throws EmployeeServiceException
 	{
 		try {
-            return dao.getAllEmployees();
+			logger.trace("Entering service layer getAllEmployees()");
+            return employeeDao.getAllEmployees();
         } catch (EmployeeDaoException e) {
-            throw new EmployeeServiceException("Service layer failed to fetch employee details.", e);
+            throw new EmployeeServiceException(e.getMessage(), e.getErrorCode(), e);
         }
 	}
 	
 	public Employee findEmployeeById(int id) throws EmployeeServiceException, EmployeeNotFoundException {
         try {
-            return dao.getEmployeeById(id);
+            return employeeDao.getEmployeeById(id);
         } catch (EmployeeDaoException e) {
-            throw new EmployeeServiceException("Service layer failed to fetch employee.", e);
+            throw new EmployeeServiceException(e.getMessage(), e.getErrorCode(), e);
         }
     }
 
@@ -51,9 +55,9 @@ public class EmployeeService {
 	{
 
 		try{
-			return dao.deleteEmployeeData(empId);
+			return employeeDao.deleteEmployeeData(empId);
 		} catch (EmployeeDaoException e) {
-            throw new EmployeeServiceException("Service layer failed to delete employee.", e);
+            throw new EmployeeServiceException(e.getMessage(), e.getErrorCode(), e);
         }
 		
 	}
@@ -63,42 +67,23 @@ public class EmployeeService {
 	{
 
 		try{
-			return dao.updateEmployee(emp);
+			return employeeDao.updateEmployee(emp);
 		} catch (EmployeeDaoException e) {
-            throw new EmployeeServiceException("Service layer failed to update employee details.", e);
+            throw new EmployeeServiceException(e.getMessage(), e.getErrorCode(), e);
         }
 	}
 	
 	
-	public List<Employee> getDataFromTextFile(List<String> lines) throws IOException 
+	public List<Integer> getAllIds() throws EmployeeServiceException 
 	{
-		List<Employee> employees = new ArrayList<Employee>();
-	
-	    for(String line : lines)
-	    {
-	    	
-	    	String[] parts = TextFileUtil.splitByDelimiter("\\$", line);    	
-	        Employee emp = new Employee(parts[0], parts[1], parts[2], 
-					   					parts[3], parts[4], parts[5], parts[6]);
-	        employees.add(emp);
-	    }
-	    return employees;
+		try{
+			return employeeDao.getAllIds();
+		} catch (EmployeeDaoException e) {
+            throw new EmployeeServiceException(e.getMessage(), e.getErrorCode(), e);
+        }
+			
 	}
-	
-	private List<Integer> getAllIds() 
-	{
 
-		return dao.getAllIds();
-	}
-	
-	
-	
-	public boolean isDuplicateId(String id) throws NumberFormatException
-	{
-		List<Integer> ids = getAllIds();
-    	return ids.contains(Integer.parseInt(id));
-    		
-	}
 	
 	
 	public boolean checkIfDataValid(Employee emp) throws EmployeeServiceException
@@ -128,11 +113,12 @@ public class EmployeeService {
 			
 			ValidationUtil.isValidActiveStatus(emp.getActiveStatus());
 
+			
 			return true;
 		}
 		catch(ValidationException e)
 		{
-			throw new EmployeeServiceException("Service layer failed to insert employee data.", e);
+			throw new EmployeeServiceException(e.getMessage(), 201, e);
 		}
 		
 		
