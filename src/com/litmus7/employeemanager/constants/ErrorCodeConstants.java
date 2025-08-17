839ddd6495e564a3045d1bfafff2852935bc74d9
Sruthy K S <141996444+SruthyK-S@users.noreply.github.com>
2025-08-17 20:32:07 +0530

    Update ErrorCodeConstants.java

--- src/com/litmus7/employeemanager/constants/ErrorCodeConstants.java
+++ src/com/litmus7/employeemanager/constants/ErrorCodeConstants.java
@@ -11,8 +11,7 @@ public class ErrorCodeConstants {
         ERROR_CODES.put(404, "Database connection error");
         ERROR_CODES.put(102, "SQL execution error");
         ERROR_CODES.put(103, "Invalid input data format");
-        ERROR_CODES.put(104, "Duplicate employee ID");
         ERROR_CODES.put(201, "Validation Error");
-        ERROR_CODES.put(202, "Error processing retrieved data");
     }
+
 }
