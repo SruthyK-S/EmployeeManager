b30363af666df068b6c831ef22cec84112b393e4
Sruthy K S <141996444+SruthyK-S@users.noreply.github.com>
2025-08-17 20:24:51 +0530

    Update EmployeeManagerApp.java

--- src/com/litmus7/employeemanager/app/EmployeeManagerApp.java
+++ src/com/litmus7/employeemanager/app/EmployeeManagerApp.java
@@ -11,9 +11,8 @@ public class EmployeeManagerApp {
 	public static void main(String[] args) 
 	{
 		
-		final String inputFilePath = "employees.txt";
 		Scanner scn = new Scanner(System.in);  
-		EmployeeController controller = new EmployeeController();
+		EmployeeController employeeController = new EmployeeController();
 		
 		String option;
 		System.out.println("Employee Management App");
@@ -22,15 +21,13 @@ public class EmployeeManagerApp {
 		while(!exit)
 		{
 			System.out.println("\nChoose one service");
-			System.out.println("1 - Read data from text file"
-					+ "\n2 - Add the read data to database"
-					+ "\n3 - Create an employee"
-					+ "\n4 - Get all employee data"
-					+ "\n5 - Get an employee data"
-					+ "\n6 - Delete an employee data"
-					+ "\n7 - Update an employee data"
-					+ "\n8 - Quit"
-					);
+			System.out.println(	  "\n1 - Create an employee"
+								+ "\n2 - Get all employee data"
+								+ "\n3 - Get an employee data"
+								+ "\n4 - Delete an employee data"
+								+ "\n5 - Update an employee data"
+								+ "\n6 - Quit"
+								);
 			
 		    option = scn.next();  
 		    switch(option) {
@@ -38,89 +35,84 @@ public class EmployeeManagerApp {
 		    
 			    case "1":
 			    {
+			    	System.out.println("Enter ID: ");
+			    	String ID = scn.next();
+			    	Response<Void> idValid = employeeController.isDuplicateId(ID);
+			    	if(idValid.getStatus() == 200)
+			    	{
+			    		System.out.println(idValid.getMessage());
+			    		break;
+			    	}
+			    	
+			    	System.out.println("Enter the employee details: ");
+			    	System.out.println("First Name : ");
+			    	String firstName = scn.next();
+			    
+			    	
+			    	System.out.println("Last Name : ");
+			    	String lastName = scn.next();
+			    	
+			    	System.out.println("Mobile Number : ");
+			    	String mobile = scn.next();
+			    	
+			    	System.out.println("Email address : ");
+			    	String email = scn.next();
+			    	
+			    	System.out.println("Joining date(yyyy-MM-dd) : ");
+			    	String date = scn.next();
 
-			    	Response<String> result = controller.getDataFromTextFile(inputFilePath);
-			    	if(result.getStatus() != 200)
-			    		System.out.println(result.getMessage());
-			    	else
-			    		System.out.println(result.getData());
-			    	break;
+			        
+			    	
+			    	System.out.println("Active Status(true/false) : ");
+			    	String activeStatus = scn.next();
+
+			    	Employee emp = new Employee(ID, firstName, lastName, mobile, email, date, activeStatus);
+			    	Response<Integer> result = employeeController.createEmployee(emp);
+			    	
+		    		System.out.println(result.getMessage());
+		    		break;
 			    	
 			    }
 			    case "2": 
 			    {
-			    	Response<Integer> result = controller.writeDataToDatabase();
-			    	System.out.println(result.getMessage());
+			    	Response<String> result = employeeController.getAllEmployees();
+			    	if(result.getStatus() == 200)
+			    		System.out.println(result.getData());
+			    	else
+			    		System.out.println(result.getMessage());
 			    	break;
 			    }
 			    case "3":
 		    	{
-			    	System.out.println("Enter the employee details: ");
-			    	System.out.println("ID : ");
+		    		System.out.println("Enter the ID: ");
 			    	String ID = scn.next();
-			    	
-			    	System.out.println("First Name : ");
-			    	String firstName = scn.next();
-			    
-			    	
-			    	System.out.println("Last Name : ");
-			    	String lastName = scn.next();
-			    	
-			    	System.out.println("Mobile Number : ");
-			    	String mobile = scn.next();
-			    	
-			    	System.out.println("Email address : ");
-			    	String email = scn.next();
-			    	
-			    	System.out.println("Joining date(yyyy-MM-dd) : ");
-			    	String date = scn.next();
-
-			        
-			    	
-			    	System.out.println("Active Status(true/false) : ");
-			    	String activeStatus = scn.next();
-
-			    	Employee emp = new Employee(ID, firstName, lastName, mobile, email, date, activeStatus);
-			    	Response<Integer> result = controller.createEmployee(emp);
-			    	
-		    		System.out.println(result.getMessage());
-		    		break;
-		    	}
-			    case "4":
-			    {
-			    	Response<String> result = controller.getAllEmployees();
+			    	Response<String> result = employeeController.getEmployeeById(ID);
 			    	if(result.getStatus() == 200)
 			    		System.out.println(result.getData());
 			    	else
 			    		System.out.println(result.getMessage());
+			    	break;
+		    	}
+			    case "4":
+			    {
+			    	System.out.println("Enter the ID of employee to be deleted: ");
+			    	String ID = scn.next();
+			    	Response<Integer> result = employeeController.deleteEmployeeById(ID);
+			    	System.out.println(result.getMessage());	
 			    	break;
 			    }
 			    case "5":
 			    {
-			    	System.out.println("Enter the ID: ");
-			    	String ID = scn.next();
-			    	Response<String> result = controller.getEmployeeById(ID);
-			    	if(result.getStatus() == 200)
-			    		System.out.println(result.getData());
-			    	else
-			    		System.out.println(result.getMessage());
-			    	break;
-			    }
-			    case "6":
-			    {
-			    	System.out.println("Enter the ID of employee to be deleted: ");
-			    	String ID = scn.next();
-			    	Response<Integer> result = controller.deleteEmployeeById(ID);
-			    	System.out.println(result.getMessage());	
-			    	break;
 			    	
-			    }
-			    case "7":
-			    {
+			    	System.out.println("Enter ID of employee to be updated: ");
+			    	String ID = scn.next();
+			    	Response<Void> idValid = employeeController.isDuplicateId(ID);
+			    	if(idValid.getStatus() != 200)
+			    	{
+			    		System.out.println(idValid.getMessage());
+			    		break;
+			    	}
 			    	System.out.println("Enter the employee details: ");
-			    	System.out.println("ID : ");
-			    	String ID = scn.next();
-			    	
 			    	System.out.println("First Name : ");
 			    	String firstName = scn.next();
 			    
@@ -144,7 +136,7 @@ public class EmployeeManagerApp {
 			    	
 			    	Employee emp = new Employee(ID, firstName, lastName, mobile, email, date, activeStatus);
 			    	
-			    	Response<Integer> result = controller.updateEmployee(emp);
+			    	Response<Integer> result = employeeController.updateEmployee(emp);
 			    	
 			    	System.out.println(result.getMessage());	
 			    	break;
